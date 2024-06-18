daeb08f673e2314992989ceb8ccd749ebfaad413
Juan Ortiz <camithe0016@outlook.es>
2024-06-18 15:36:29 -0500

    nw rls

--- Js/projects.js
+++ Js/projects.js
@@ -6,7 +6,7 @@
    
     
     const $itemList = document.querySelectorAll(".list-category");
-    const $itemProject = document.querySelectorAll(".item-project");
+    const $itemProject = document.querySelectorAll(".project-item");
     
     $itemList.forEach((button)=>{
         button.addEventListener("click",(e)=>{
@@ -16,15 +16,15 @@
 
             $itemProject.forEach((item)=>{
 
-                if(filter == "TODOS"){
-                    item.style.display = "flex"
+                if(filter =="TODOS"){
+                    item.style.display ="flex"
                 } else{
 
                     if(item.classList.contains(filter)){
-                        item.style.display = "flex"
+                        item.style.display ="flex"
 
                     }else{
-                        item.style.display = "none"
+                        item.style.display ="none"
                     }
                 }
             })
