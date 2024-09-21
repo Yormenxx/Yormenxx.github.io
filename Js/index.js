5c3d2c402183211a34fbaececf2ce26c38809a98
Juan Ortiz <camithe0016@outlook.es>
2024-09-20 20:47:48 -0500

    nw rls

--- Js/index.js
+++ Js/index.js
@@ -10,6 +10,10 @@ function update (){
     requestAnimationFrame(update)
 }
 
+//Slider
+
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
 
 //CHEVRON UP
 
@@ -34,6 +38,54 @@ window.onscroll = () =>{
     }
 
 }
+
+
+
+let $cont1 = document.getElementById("cont1"),
+    $cont2 = document.getElementById("cont2"),
+    $cont3 = document.getElementById("cont3")
+
+let cant1 = 0,cant2 = 0,cant3 = 0,cant4 = 0
+let time = 25
+
+let tiempo1 = setInterval(() => {
+
+    $cont1.textContent = cant1+=1
+
+    if(cant1 === 60)
+
+        {
+            clearInterval(tiempo1)
+        }
+    
+}, 100);
+
+
+let tiempo2 = setInterval(() => {
+
+    $cont2.textContent = cant2+=1
+
+    if(cant2 === 100)
+
+        {
+            clearInterval(tiempo2)
+        }
+    
+}, 50);
+
+
+
+let tiempo3 = setInterval(() => {
+
+    $cont3.textContent = cant3+=1
+
+    if(cant3 === 6)
+
+        {
+            clearInterval(tiempo3)
+        }
+    
+}, 200);
 
 //PROJECT FILTER
 
